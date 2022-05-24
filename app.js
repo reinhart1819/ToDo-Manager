eae09084dc59a86c1c723cc9e60a4e654e1124db
Christopher Reinhart <reinhart1819@gmail.com>
2022-05-23 18:28:33 -0700

    Finished project

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
+let newTask= document.getElementById("txtTask");
+let count = 0;
 function registerTask() {
     // get info from input
 
     // display info on console--- then on a html element
-
-    let task= document.getElementById("txtTask").value;
-  
-
-    console.log(task);
+    console.log(newTask.value);
+    let tmp= `<li id="${count}" class="task">${newTask.value} <button onclick="deleteTask(${count})">🗑️
+    </button></li>`;
+    document.getElementById("todoList").innerHTML+=tmp;
+    clearForm();
+    count++
 }
 
 
-function deleteTask() {
+function deleteTask(x) {
+    document.getElementById(x).remove()
 
+}
+
+function clearForm() {
+    newTask.value="";
 }
